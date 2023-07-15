2aed422338fd99b8a1e01b30dcdab0a2c37f23b9
amalali8 <amalza958@gmail.com>
2023-07-15 03:59:37 +0300

    answer

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let num1 = 1;
+let num2 = 9;
+
+
+if (num1 > num2) {
+  console.log(`The largest number out of the two numbers is ${num1}`);
+} else if (num2 > num1) {
+  console.log(`The largest number out of the two numbers is ${num2}`);
+} else {
+  console.log("Both numbers are equal");
+}
